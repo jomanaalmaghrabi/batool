85a05c9d6eea80c670252327213c63dd59f5fc05
1910769 <1910769@612b61l10p20.kaau.edu.sa>
2022-02-06 10:34:26 +0300

    jomana my first lap

---
+++ jomanalap1/src/jomanalap1/Jomanalap1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jomanalap1;
+
+/**
+ *
+ * @author 1910769
+ */
+public class Jomanalap1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
